2dfe9261aa7050c8025713444ce46d52bbd8c5db
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-17 22:54:51 +0200

    Updates

---
+++ precache-manifest.88481f8dad5ce6ab4eb19b91f5a30722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0958b3c33c2f4f83cb167b328506e10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de06fc43da5ce30e45c0",
+    "url": "./static/css/main.dd1d04af.chunk.css"
+  },
+  {
+    "revision": "474b02707ae134eb723b",
+    "url": "./static/js/2.ae916442.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.ae916442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de06fc43da5ce30e45c0",
+    "url": "./static/js/main.74a5d59a.chunk.js"
+  },
+  {
+    "revision": "63d63249b359e20c019c",
+    "url": "./static/js/runtime-main.74d63094.js"
+  }
+]);
